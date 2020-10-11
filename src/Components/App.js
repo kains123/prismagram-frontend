01543fc3d842906fd07fe583543a3adf5819d5bc
kains123 <kains0417@naver.com>
2020-10-11 20:38:50 +0900

    Modified logIn

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { gql } from "apollo-boost";
 import styled, { ThemeProvider } from "styled-components";
+import { useQuery } from '@apollo/client';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import GlobalStyles from "../Styles/GlobalStyles";
 import Theme from "../Styles/Theme";
 import Router from "./Router";
-import { useQuery } from '@apollo/client';
 //주의 해야한다.....!!!! 이 부분 꼭 기억하기 
 import Footer from "./Footer";
 
@@ -31,6 +33,7 @@ export default () => {
         <GlobalStyles />
         <Router isLoggedIn={isLoggedIn} />
         <Footer />
+        <ToastContainer position={toast.POSITION.BOTTOM_LEFT} />
       </Wrapper>
     </ThemeProvider>
   );
--- src/Routes/Auth/AuthPresenter.js
+++ src/Routes/Auth/AuthPresenter.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
-import Input from "../Components/Input";
-import Button from "../Components/Button";
-import useInput from "../Hooks/useInput";
+import Input from "../../Components/Input";
+import Button from "../../Components/Button";
+
 
 const Wrapper = styled.div`
   min-height: 80vh;
@@ -47,34 +47,39 @@ const Form = styled(Box)`
   }
 `;
 
-
-export default () => {
-    const [action, setAction] = useState("logIn");
-    const username = useInput("");
-    const password = useInput("");
-    const firstName = useInput("");
-    const lastName = useInput("");
-    const email = useInput("");
-
-    return (
+export default ({
+    action,
+    name,
+    firstName,
+    lastName,
+    email,
+    setAction,
+    onSubmit,
+    secret
+}) => (
         <Wrapper>
             <Form>
-                {action === "logIn" ? (
-                    <form>
-                        <Input placeholder={"Username"} {...username} />
-                        <Input placeholder={"Password"} {...password} type="password" />
+                {action === "logIn" && (
+                    <form onSubmit={onSubmit}>
+                        <Input placeholder={"Email"} {...email} type="email" />
                         <Button text={"Log in"} />
                     </form>
-                ) : (
-                        <form>
-                            <Input placeholder={"First name"} {...firstName} />
-                            <Input placeholder={"Last name"} {...lastName} />
-                            <Input placeholder={"Email"} {...email} type="email" />
-                            <Input placeholder={"Username"} {...username} />
-                            <Input placeholder={"Password"} {...password} type="password" />
-                            <Button text={"Sign up"} />
-                        </form>
-                    )}
+                )}
+                {action === "signUp" && (
+                    <form onSubmit={onSubmit}>
+                        <Input placeholder={"First name"} {...firstName} />
+                        <Input placeholder={"Last name"} {...lastName} />
+                        <Input placeholder={"Email"} {...email} type="email" />
+                        <Input placeholder={"Username"} {...name} />
+                        <Button text={"Sign up"} />
+                    </form>
+                )}
+                {action === "confirm" && (
+                    <form onSubmit={onSubmit}>
+                        <Input placeholder="Paste your secret" required {...secret} />
+                        <Button text={"Confirm"} />
+                    </form>
+                )}
             </Form>
             <StateChanger>
                 {action === "logIn" ? (
@@ -91,4 +96,3 @@ export default () => {
             </StateChanger>
         </Wrapper>
     );
-};
